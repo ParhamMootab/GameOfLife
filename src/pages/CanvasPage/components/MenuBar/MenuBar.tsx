65e1011bc63d7094b880bbeb81db66c0788a43e8
Parham Shagerdmootab <psm1379@gmail.com>
2024-05-16 08:19:06 -0700

    Creating Menu bar

---
+++ src/pages/CanvasPage/components/MenuBar/MenuBar.tsx
@@ -0,0 +1,19 @@
+import React, { useState, useEffect, useRef, FC } from 'react';
+import './MenuBarStyles.css'
+
+
+export const MenuBar : FC = () => {
+    
+
+    return <>
+        <div className="menubar">
+            <button></button>
+            <button></button>
+            
+            <button>Start</button>
+            <button>Pause</button>
+            <button>Reset</button>
+            
+        </div>
+    </>
+}
--- src/pages/CanvasPage/index.tsx
+++ src/pages/CanvasPage/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
-import { MenuBar } from "./components/MenuBar";
-import GameGrid from "./components/Grid";
+import { MenuBar } from "./components/MenuBar/MenuBar";
+import GameGrid from "./components/Grid/Grid";
 
 export const CanvasPage: React.FC = () => {
   
